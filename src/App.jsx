af92d95bc6723b0fc91b29bb537a10fe5661e407
Subasre K <subasrek@gmail.com>
2023-09-01 09:46:16 +0530

    initial project update

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Home from './Component/Home'
+import HomeClass from './Component/HomeClass'
+import About from './Component/About'
+import AboutClass from './Component/AboutClass'
+import RenderingComponent from './Component/RenderingComponent'
+import LifeCycleComponent from './Component/LifeCycleComponent'
+import  DesignComponent  from './Component/DesignComponent' 
+
+function App() {
+  return (
+    <div>
+<About name="App Component" work="Parent component"/>
+<AboutClass name="App component" work="parent component"/>
+  
+     
+
+    </div>
+
+
+  )
+}
+
+
+
+export default App
+  
